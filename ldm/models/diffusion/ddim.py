465d4454b8d3b298fc54ffc65570577d65594bb3
Muhammad Islam <mrislam@terpmail.umd.edu>
2025-02-12 01:16:09 -0500

    Produced and saved pred_x0 imgs

--- ldm/models/diffusion/ddim.py
+++ ldm/models/diffusion/ddim.py
@@ -249,7 +249,7 @@ class DDIMSampler(object):
         if noise_dropout > 0.:
             noise = torch.nn.functional.dropout(noise, p=noise_dropout)
         x_prev = a_prev.sqrt() * pred_x0 + dir_xt + noise
-        return x_prev, pred_x0
+        return x_prev, pred_x0 
 
     @torch.no_grad()
     def encode(self, x0, c, t_enc, use_original_steps=False, return_intermediates=None,
